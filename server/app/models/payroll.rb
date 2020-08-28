30828574b4166b5c2f7d7669290f069537013964
Jason Stathopulos <jstathopulos@gmail.com>
2020-08-28 18:56:30 -0400

    Add Method Add Data From CSV to Payroll Model
    Add the method Add Data From CSV to the Payroll model since the user has
    the option of uploading payrolls to the server and populating the
    database from the frontend.
    
    ---
    
    Ref:
    
    https://collectiveidea.com/blog/archives/2017/01/16/testing-an-uploaded-file-in-rspec
    http://phrogz.net/filename-without-extension-ruby

--- server/app/models/payroll.rb
+++ server/app/models/payroll.rb
@@ -17,4 +17,27 @@ class Payroll < ApplicationRecord
 
   alias_attribute :identifier, :payroll_id
   alias_attribute :date, :payroll_date
+
+  def self.add_data_from_csv(file)
+    payroll_id = Payroll.get_payroll_id_from_filename(file)
+
+    CSV.foreach(file.path, headers: :first_row) do |row|
+      employee = Employee.find_by_employee_id(row['employee id'])
+      job_group = JobGroup.find_by_name(row['job group'])
+
+      Payroll.create!(
+        employee: employee,
+        job_group: job_group,
+        payroll_date: row['date'],
+        hours: row['hours worked'],
+        payroll_id: payroll_id
+      )
+    end
+  end
+
+  private
+
+  def self.get_payroll_id_from_filename(file)
+    File.basename(file.original_filename, ".*").split('-').last.to_i
+  end
 end
--- server/spec/models/payroll_spec.rb
+++ server/spec/models/payroll_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Payroll, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:file) { fixture_file_upload('time-report-79.csv') }
+
+  describe '#add_data_from_csv' do
+    context 'when processing a payroll from CSV format' do
+      before(:each) do
+        create(:employee, employee_id: '2')
+        create(:job_group, name: 'A')
+        create(:job_group, name: 'B')
+      end
+
+      context 'on adding rows' do
+        it 'returns nothing' do
+          create(:employee, employee_id: '1')
+
+          Payroll.add_data_from_csv(file)
+
+          first_payroll = Payroll.first
+
+          expect(Payroll.count).to eq(9)
+          expect(first_payroll.identifier).to eq(79)
+          expect(first_payroll.hours).to eq(7.5)
+          expect(first_payroll.date.to_s).to eq('2016-11-14')
+          expect(first_payroll.employee.identifier).to eq('1')
+          expect(first_payroll.job_group.name).to eq('A')
+        end
+      end
+    end
+  end
 end
